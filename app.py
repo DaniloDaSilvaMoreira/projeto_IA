37c717697dc9df41de03559e76337bf0199a8fe2
unknown <danilosilva@unitins.br>
2024-08-29 09:17:21 -0300

    atualização

---
+++ app.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 28 02:21:09 2019
+
+Algoritmo Genético
+
+@author: Marco
+"""
+
+from AlgoritmoGeneticoSudoku import AlgoritmoGenetico as AG
+
+ag = AG(5, 16, 4)
+
+ag.operadores_geneticos()
